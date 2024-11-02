421e889066baab1250bf11d87b371908b3ef1e1f
tachorzy <tachorzyad@gmail.com>
2024-11-02 16:47:58 -0500

    chore: added Switzer font

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { SwitzerMedium } from "../lib/localFont";
 
 const Navbar = () => {
     return(
         <div className="absolute top-0 left-0 mt-16 ml-16 flex flex-row gap-x-2 text-4xl align-baseline">
             <Image src="/icons/FleetSignalLogo.svg" width={40} height={40} alt="FleetSignal logo"/>
-            <h1 className="font-medium text-[#1E1E1E]">FleetSignal</h1>
+            <h1 className={SwitzerMedium.className + " font-medium text-[#1E1E1E]"}>FleetSignal</h1>
         </div>
 
     )
---
+++ frontend/lib/localFont.ts
@@ -0,0 +1,19 @@
+import localFont from 'next/font/local';
+
+export const SwitzerBold = localFont({
+    src: '../public/fonts/switzer/Switzer-Bold.otf',
+    variable: '--font-switzer-bold',
+    weight: '400 900',
+});
+
+export const SwitzerRegular = localFont({
+    src: '../public/fonts/switzer/Switzer-Regular.otf',
+    variable: '--font-switzer-regular',
+    weight: '400 900',
+});
+
+export const SwitzerMedium = localFont({
+    src: '../public/fonts/switzer/Switzer-Medium.otf',
+    variable: '--font-switzer-medium',
+    weight: '400 900',
+});
