5c50a5ba8cb2d1d63fd30715aa2ade056827389b
tachorzy <tachorzyad@gmail.com>
2024-11-02 16:35:28 -0500

    feat: added Navbar component

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const Navbar = () => {
+    return(
+        <div className="absolute top-0 left-0 mt-16 ml-16 flex flex-row gap-x-2 text-4xl align-baseline">
+            <Image src="/icons/FleetSignalLogo.svg" width={40} height={40} alt="FleetSignal logo"/>
+            <h1 className="font-medium text-[#1E1E1E]">FleetSignal</h1>
+        </div>
+
+    )
+};
+
+export default Navbar;
