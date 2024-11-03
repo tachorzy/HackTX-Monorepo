efea6a8fe167354f67cec435de391d11a3266dc0
tachorzy <tachorzyad@gmail.com>
2024-11-02 19:09:33 -0500

    refactor: adjusted SearchBarWrapper

--- frontend/components/searchbar/SearchBar.tsx
+++ frontend/components/searchbar/SearchBar.tsx
@@ -6,12 +6,12 @@ import SearchBarButton from "./SearchBarButton";
 
 const SearchBar = () => {
     return(
-        <div className={SwitzerRegular.className + " flex flex-row gap-x-4 p-2 rounded-full w-1/3 text-xl text-white placeholder-white dark:text-white bg-gradient-to-br from-[#13F55F] to-[#098AAD]"}>
+        <div className={SwitzerRegular.className + " flex flex-row gap-x-4 rounded-full p-2 w-1/3 text-xl text-white placeholder-white dark:text-white bg-gradient-to-br from-[#13F55F] to-[#098AAD]"}>
             <Image src="/icons/aircraft.svg" width={24} height={24} alt="plane image" className="select-none float-left justify-center self-center ml-5 mt-0.5"/>
             <input
                 type="text"
                 placeholder="Enter your airframe code"
-                className="text-xl text-white placeholder-white dark:text-white bg-transparent w-full"
+                className="text-xl text-white placeholder-white dark:text-white bg-transparent w-full p-2"
                 onChange={() => {console.log("Search bar changed")}}
             />
             <SearchBarButton/>
